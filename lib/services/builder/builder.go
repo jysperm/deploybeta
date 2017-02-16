de44015e4a6f4cc18bc14fb16826af466e131687
Mason Zhang <zzy228961125@gmail.com>
2017-02-17 01:31:05 +0800

    :ok_hand: Adding returing value of Image ID & Improving unit test

--- lib/services/builder/builder.go
+++ lib/services/builder/builder.go
@@ -1,6 +1,7 @@
 package builder
 
 import (
+	"bytes"
 	"io"
 	"os"
 
@@ -9,11 +10,12 @@ import (
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/pkg/archive"
 	"github.com/docker/docker/pkg/gitutils"
+	"github.com/docker/docker/pkg/jsonmessage"
 	"github.com/pkg/errors"
 	"golang.org/x/net/context"
 )
 
-func pullRepository(url string) (string, error) {
+func cloneRepository(url string) (string, error) {
 	path, err := gitutils.Clone(url)
 	if err != nil {
 		return "", err
@@ -21,7 +23,7 @@ func pullRepository(url string) (string, error) {
 	return path, nil
 }
 
-func tarRepository(path string) (io.ReadCloser, error) {
+func buildContext(path string) (io.ReadCloser, error) {
 	content, err := archive.Tar(path, archive.Gzip)
 	if err != nil {
 		return nil, err
@@ -31,49 +33,55 @@ func tarRepository(path string) (io.ReadCloser, error) {
 }
 
 //BuildImage will build a docker image accroding to the repo's url and depth and Dockerfiles
-func BuildImage(opts types.ImageBuildOptions, url string) error {
+func BuildImage(opts types.ImageBuildOptions, url string) (string, error) {
 	switch {
 	case opts.Tags == nil:
-		return errors.New("Need one or more tags")
+		return "", errors.New("Need one or more tags")
 	case opts.Dockerfile == "":
-		return errors.New("Need a name of Dockerfile")
+		return "", errors.New("Need a name of Dockerfile")
 	}
-	opts.NoCache = true
+	opts.NoCache = false
 	opts.Remove = true
-	opts.ForceRemove = true
 	opts.SuppressOutput = true
 	opts.Isolation = ""
 
 	client, err := client.NewEnvClient()
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	ctx := context.Background()
 
-	dirPath, err := pullRepository(url)
+	dirPath, err := cloneRepository(url)
 	if err != nil {
-		return err
+		return "", err
 	}
 
-	content, err := tarRepository(dirPath)
+	content, err := buildContext(dirPath)
 	defer content.Close()
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	buildCtx, relDockerfile, err := dockerbuilder.GetContextFromReader(content, opts.Dockerfile)
 	if err != nil {
-		return err
+		return "", err
 	}
 	opts.Dockerfile = relDockerfile
 	response, err := client.ImageBuild(ctx, buildCtx, opts)
 	defer response.Body.Close()
 	if err != nil {
-		return err
+		return "", err
 	}
 
+	buildBuf := bytes.NewBuffer(nil)
+	err = jsonmessage.DisplayJSONMessagesStream(response.Body, buildBuf, os.Stdout.Fd(), false, nil)
+	if err != nil {
+		return "", err
+	}
+
+	imageID := buildBuf.String()[7:19]
 	defer os.RemoveAll(dirPath)
 
-	return nil
+	return imageID, nil
 }
--- lib/services/builder/builder_test.go
+++ lib/services/builder/builder_test.go
@@ -7,14 +7,30 @@ import (
 )
 
 func TestBuildImage(t *testing.T) {
+
 	opts := types.ImageBuildOptions{
 		Tags:       []string{"docker-test"},
 		Dockerfile: "Dockerfile",
 	}
 
-	err := BuildImage(opts, "https://github.com/mason96112569/docker-test.git")
+	id, err := BuildImage(opts, "https://github.com/mason96112569/docker-test.git")
 	if err != nil {
 		t.Error(err)
+	}
+	t.Log("Image ID is " + id)
+}
+
+func TestBuildFailure(t *testing.T) {
+	opts := types.ImageBuildOptions{
+		Tags:       []string{"failure"},
+		Dockerfile: "Dockerfile",
+	}
+
+	_, err := BuildImage(opts, "https://github.com/mason96112569/docker-test-failure.git")
+	if err != nil {
+		t.Log(err.Error())
+	} else {
+		t.Error("It should have an error")
 	}
 
 }

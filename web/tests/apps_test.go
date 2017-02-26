c74fdad88d90b4dba0fa54aaf65b0f5866910f03
jysperm <jysperm@gmail.com>
2017-02-26 16:52:21 +0800

    :white_check_mark: Add tests for POST /apps (gorequest)

---
+++ web/tests/apps_test.go
@@ -0,0 +1,59 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/jysperm/deploying/config"
+	accountModel "github.com/jysperm/deploying/lib/models/account"
+	appModel "github.com/jysperm/deploying/lib/models/app"
+	sessionModel "github.com/jysperm/deploying/lib/models/session"
+	. "github.com/jysperm/deploying/lib/testing"
+	"github.com/jysperm/deploying/lib/utils"
+	"github.com/jysperm/deploying/web"
+)
+
+func init() {
+	go web.CreateWebServer().Start(config.Port)
+}
+
+func TestCreateApp(t *testing.T) {
+	account, _ := SeedAccount()
+	session := SeedSession(&account)
+	appName := utils.RandomString(10)
+
+	resBody := appModel.Application{}
+
+	res, _, errs := Request("POST", "/apps").
+		Set("Authorization", session.Token).
+		SendStruct(map[string]string{
+			"name": appName,
+		}).EndStruct(&resBody)
+
+	t.Log("Created app", resBody)
+
+	if len(errs) != 0 {
+		t.Error(errs)
+	}
+
+	if res.StatusCode != 201 {
+		t.Errorf("res.StatusCode %v", res.StatusCode)
+	}
+
+	res, _, errs = Request("POST", "/apps").
+		Set("Authorization", session.Token).
+		SendStruct(map[string]string{
+			"name": appName,
+		}).End()
+
+	if len(errs) != 0 {
+		t.Error(errs)
+	}
+
+	if res.StatusCode != 409 {
+		t.Errorf("res.StatusCode %v", res.StatusCode)
+	}
+
+	accountModel.DeleteByName(session.Username)
+	sessionModel.DeleteByToken(session.Token)
+	appModel.DeleteByName(appName)
+}

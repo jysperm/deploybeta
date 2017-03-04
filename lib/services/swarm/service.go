9b052439e8861dc91d860fd43a27ce3da585b1c1
Mason Zhang <zzy228961125@gmail.com>
2017-03-04 23:29:38 +0800

    :hammer: Refactoring the package of swarm

--- lib/services/swarm/service.go
+++ lib/services/swarm/service.go
@@ -1,54 +1,188 @@
 package swarm
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+
 	"github.com/docker/docker/api/types"
-	dockerSwarm "github.com/docker/docker/api/types/swarm"
+	"github.com/docker/docker/api/types/swarm"
+	"github.com/docker/docker/client"
+
+	"github.com/jysperm/deploying/lib/models/app"
+	"github.com/jysperm/deploying/lib/services"
+	"github.com/jysperm/deploying/lib/services/builder"
+
 	"golang.org/x/net/context"
 )
 
-//CreateService will create a new app and return its id and warnings
-func CreateService(spec dockerSwarm.ServiceSpec, options types.ServiceCreateOptions) (string, []string, error) {
-	ctx := context.Background()
-
-	response, err := swarmClient.ServiceCreate(ctx, spec, options)
-	if err != nil {
-		return "", []string{}, err
-	}
-
-	return response.ID, response.Warnings, nil
+//UpstreamConfig define the structure of upstream config
+type UpstreamConfig struct {
+	Port uint32 `json:"port"`
 }
 
-//RemoveService will remove a existed app
-func RemoveService(serviceID string) error {
-	ctx := context.Background()
+var swarmClient *client.Client
 
-	if err := swarmClient.ServiceRemove(ctx, serviceID); err != nil {
+func init() {
+	var err error
+	swarmClient, err = client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+}
+
+//CreateService will build a image from Dockerfile and deploy a service
+func CreateService(app app.Application) error {
+	versionTag := fmt.Sprintf("%s:%s", app.Name, app.Version)
+	buildOpts := types.ImageBuildOptions{
+		Tags: []string{versionTag},
+	}
+	shasum, err := builder.BuildImage(buildOpts, app.GitRepository)
+	if err != nil {
+		return err
+	}
+
+	uint64Instances := uint64(app.Instances)
+	imageName := fmt.Sprintf("%s@sha256:%s", versionTag, shasum)
+	containerSpec := swarm.ContainerSpec{Image: imageName}
+	taskSpec := swarm.TaskSpec{ContainerSpec: containerSpec}
+	replicatedService := swarm.ReplicatedService{Replicas: &uint64Instances}
+	serviceMode := swarm.ServiceMode{Replicated: &replicatedService}
+	portConfig := swarm.PortConfig{
+		Protocol:    swarm.PortConfigProtocolTCP,
+		TargetPort:  3000,
+		PublishMode: swarm.PortConfigPublishModeIngress,
+	}
+	endpointSpec := swarm.EndpointSpec{
+		Mode:  "vip",
+		Ports: []swarm.PortConfig{portConfig},
+	}
+	serviceSpec := swarm.ServiceSpec{
+		Annotations:  swarm.Annotations{Name: app.Name},
+		TaskTemplate: taskSpec,
+		Mode:         serviceMode,
+		EndpointSpec: &endpointSpec,
+	}
+	serviceResponse, err := swarmClient.ServiceCreate(context.Background(), serviceSpec, types.ServiceCreateOptions{})
+	if err != nil {
+		return err
+	}
+
+	publishedPort, err := extractPort(serviceResponse.ID)
+	if err != nil {
+		return err
+	}
+	upstream, err := json.Marshal([]UpstreamConfig{UpstreamConfig{Port: publishedPort}})
+	if err != nil {
+		return err
+	}
+	upstreamKey := fmt.Sprintf("/upstream/%s", app.Name)
+	if _, err := services.EtcdClient.Put(context.Background(), upstreamKey, string(upstream)); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-//UpdateService will update the config of a existed app and return warnings of updating opterations
-func UpdateService(serviceID string, version dockerSwarm.Version, spec dockerSwarm.ServiceSpec, options types.ServiceUpdateOptions) ([]string, error) {
-	ctx := context.Background()
-
-	response, err := swarmClient.ServiceUpdate(ctx, serviceID, version, spec, options)
+//UpdateService will updaate the config of given app
+func UpdateService(app app.Application) error {
+	serviceID, err := extractServiceID(app.Name)
 	if err != nil {
-		return []string{}, err
+		return err
 	}
 
-	return response.Warnings, nil
+	versionTag := fmt.Sprintf("%s:%s", app.Name, app.Version)
+	buildOpts := types.ImageBuildOptions{
+		Tags: []string{versionTag},
+	}
+	shasum, err := builder.BuildImage(buildOpts, app.GitRepository)
+	if err != nil {
+		return err
+	}
+
+	uint64Instances := uint64(app.Instances)
+	imageName := fmt.Sprintf("%s@sha256:%s", versionTag, shasum)
+	containerSpec := swarm.ContainerSpec{Image: imageName}
+	taskSpec := swarm.TaskSpec{ContainerSpec: containerSpec}
+	replcatedService := swarm.ReplicatedService{Replicas: &uint64Instances}
+	serviceMode := swarm.ServiceMode{Replicated: &replcatedService}
+	portConfig := swarm.PortConfig{
+		Protocol:    swarm.PortConfigProtocolTCP,
+		TargetPort:  3000,
+		PublishMode: swarm.PortConfigPublishModeIngress,
+	}
+	endpointSpec := swarm.EndpointSpec{
+		Mode:  "vip",
+		Ports: []swarm.PortConfig{portConfig},
+	}
+	serviceSpec := swarm.ServiceSpec{
+		Annotations:  swarm.Annotations{Name: app.Name},
+		TaskTemplate: taskSpec,
+		Mode:         serviceMode,
+		EndpointSpec: &endpointSpec,
+	}
+	_, err = swarmClient.ServiceUpdate(context.Background(), serviceID, swarm.Version{}, serviceSpec, types.ServiceUpdateOptions{})
+	if err != nil {
+		return err
+	}
+
+	publishedPort, err := extractPort(serviceID)
+	if err != nil {
+		return err
+	}
+	upstream, err := json.Marshal([]UpstreamConfig{UpstreamConfig{Port: publishedPort}})
+	if err != nil {
+		return err
+	}
+	upstreamKey := fmt.Sprintf("/upstream/%s", app.Name)
+	if _, err := services.EtcdClient.Put(context.Background(), upstreamKey, string(upstream)); err != nil {
+		return err
+	}
+
+	return nil
 }
 
-//ListServices will list all services of the swarm
-func ListServices(options types.ServiceListOptions) ([]dockerSwarm.Service, error) {
-	ctx := context.Background()
-
-	services, err := swarmClient.ServiceList(ctx, options)
+//RemoveService will remove the given service
+func RemoveService(app app.Application) error {
+	serviceID, err := extractServiceID(app.Name)
 	if err != nil {
-		return []dockerSwarm.Service{}, err
+		return err
+	}
+	if err := swarmClient.ServiceRemove(context.Background(), serviceID); err != nil {
+		return err
 	}
 
-	return services, nil
+	upstreamKey := fmt.Sprintf("/upstream/%s", app.Name)
+	if _, err := services.EtcdClient.Delete(context.Background(), upstreamKey); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func extractServiceID(name string) (string, error) {
+	var serviceID string
+	services, err := swarmClient.ServiceList(context.Background(), types.ServiceListOptions{})
+	if err != nil {
+		return "", err
+	}
+
+	for _, i := range services {
+		if i.Spec.Annotations.Name == name {
+			serviceID = i.ID
+		}
+	}
+	if serviceID == "" {
+		return "", errors.New("Not found service")
+	}
+	return serviceID, nil
+}
+
+func extractPort(serviceID string) (uint32, error) {
+	service, _, err := swarmClient.ServiceInspectWithRaw(context.Background(), serviceID)
+	if err != nil {
+		return 0, nil
+	}
+	portConfig := service.Endpoint.Ports[0]
+	return portConfig.PublishedPort, nil
 }

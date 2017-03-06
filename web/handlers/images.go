b7f1f2faedbcf61828f518d25ee2d0053fd95847
Mason Zhang <zzy228961125@gmail.com>
2017-03-06 10:47:23 +0800

    :tada: First version of restful api of building images

---
+++ web/handlers/images.go
@@ -0,0 +1,51 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo"
+
+	"github.com/docker/docker/api/types"
+
+	"github.com/jysperm/deploying/lib/services/builder"
+	. "github.com/jysperm/deploying/web/handlers/helpers"
+)
+
+func generateVersion() string {
+	now := time.Now()
+	return fmt.Sprintf("%d%d%d-%d%d%d", now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second())
+}
+
+func CreateImage(ctx echo.Context) error {
+	params := map[string]string{}
+	err := ctx.Bind(&params)
+
+	if err != nil {
+		return NewHTTPError(http.StatusBadRequest, err)
+	}
+
+	version := generateVersion()
+	buildOpts := types.ImageBuildOptions{
+		Tags: []string{version},
+	}
+	shasum, err := builder.BuildImage(buildOpts, params["gitRepository"])
+	if err != nil {
+		return NewHTTPError(http.StatusBadRequest, err)
+	}
+	var image struct {
+		Shasum  string
+		Version string
+	}
+	image.Shasum = shasum
+	image.Version = version
+
+	imageJSON, err := json.Marshal(image)
+	if err != nil {
+		return NewHTTPError(http.StatusInternalServerError, err)
+	}
+
+	return ctx.JSON(http.StatusCreated, imageJSON)
+}

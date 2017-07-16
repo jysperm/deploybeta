c77b29c4fc01c01f756daa64b6a33c2b40e6a0a2
jysperm <jysperm@gmail.com>
2017-07-16 10:31:51 +0800

    :bug: Fix AppOwnerMiddleware

--- web/handlers/helpers/authenticate.go
+++ web/handlers/helpers/authenticate.go
@@ -48,9 +48,10 @@ func AppOwnerMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 		for _, app := range apps {
 			if app.Name == appName {
 				ctx.Set("app", app)
-				next(ctx)
+				return next(ctx)
 			}
 		}
+
 		return NewHTTPError(http.StatusBadRequest, errors.New("Not found application"))
 	}
 }

f8cd3312b71d027933971d802621c8f8ad8327e5
jysperm <jysperm@gmail.com>
2017-08-05 17:49:58 +0800

    :hammer: Refactor dashboard and support edit app

--- frontend/components/console.jsx
+++ frontend/components/console.jsx
@@ -1,9 +1,10 @@
+import _ from 'lodash';
+import {Row, Tabs, Tab} from 'react-bootstrap';
 import React, {Component} from 'react';
-import {Row, Tabs, Tab, Button, Table, ButtonGroup, Modal, FormGroup, ControlLabel, FormControl, HelpBlock} from 'react-bootstrap';
-import {Label, DropdownButton, MenuItem, Checkbox} from 'react-bootstrap';
 
 import {requestJson} from '../lib/request';
 import LayoutView from './layout';
+import ApplicationsTab from './console/application';
 
 export default class ConsoleView extends Component {
   constructor(props) {
@@ -27,171 +28,18 @@ export default class ConsoleView extends Component {
       <Row>
         <Tabs defaultActiveKey={1} id='pages'>
           <Tab eventKey={1} title='Applications'>
-            <ApplicationsTab apps={this.state.apps} onAppCreated={::this.onAppCreated} />
+            <ApplicationsTab apps={this.state.apps} onAppEdited={::this.onAppEdited} />
           </Tab>
         </Tabs>
       </Row>
     </LayoutView>;
   }
 
-  onAppCreated(app) {
-    this.setState({
-      apps: this.state.apps.concat(app)
-    });
-  }
-}
-
-class ApplicationsTab extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      creatingApp: false,
-      creatingAppName: '',
-
-      buildingVersion: false,
-      buildingVersionDeploy: false,
-      buildingVersionTag: 'master'
+  onAppEdited(app) {
+    if (app) {
+      this.setState({
+        apps: [app].concat(_.reject(this.state.apps, {name: app.name}))
+      });
     }
-  }
-
-  render() {
-    return <div>
-      <ButtonGroup>
-        <Button bsStyle='success' onClick={::this.onCreatingApp}>Create App</Button>
-      </ButtonGroup>
-      <Table responsive>
-        <thead>
-          <tr>
-            <th>Domain</th>
-            <th>Version</th>
-            <th>Instances</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.props.apps.map( app => {
-            return <tr key={app.name}>
-              <td>{app.name}</td>
-              <td>
-                <Label bsStyle='primary'>{app.version}</Label>
-                <ButtonGroup>
-                  <Button onClick={this.onBuildingVersion.bind(this, app.name)}>Build</Button>
-                  <DropdownButton title='Deploy...' id='deploy-dropdown'>
-                    <MenuItem eventKey={1}>20170219-205301</MenuItem>
-                  </DropdownButton>
-                </ButtonGroup>
-              </td>
-              <td>{app.instances}</td>
-              <td>
-                <ButtonGroup>
-                  <Button bsStyle='danger'>Delete</Button>
-                </ButtonGroup>
-              </td>
-            </tr>;
-          })}
-        </tbody>
-      </Table>
-
-      <Modal show={this.state.creatingApp} onHide={::this.onCreateAppModalClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Create new app</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <FormGroup controlId='app-name'>
-            <ControlLabel>Name</ControlLabel>
-            <FormControl type='text' onChange={::this.onCreatingAppNameEdited} value={this.state.creatingAppName} />
-            <HelpBlock>Used as your domain, must be globally unique</HelpBlock>
-          </FormGroup>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button bsStyle='success' onClick={::this.onCreateApp}>Create</Button>
-        </Modal.Footer>
-      </Modal>
-
-      <Modal show={this.state.buildingVersion} onHide={::this.onBuildVersionModalClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Build new version</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <FormGroup controlId='git-tag'>
-            <ControlLabel>App</ControlLabel>
-            <FormControl type='text' disabled={true} value={this.state.buildingVersion}  />
-          </FormGroup>
-          <FormGroup controlId='git-tag'>
-            <ControlLabel>Git tag</ControlLabel>
-            <FormControl type='text' onChange={::this.onBuildingVersionTagEdited} value={this.state.buildingVersionTag}  />
-            <HelpBlock>Git branch, tag or commit hash</HelpBlock>
-          </FormGroup>
-          <Checkbox onChange={::this.onBuildVersionDeployEdited} value={this.state.buildingVersionDeploy}>Deploy to app after build finished</Checkbox>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button bsStyle='success' onClick={::this.onBuildVersion}>Build</Button>
-        </Modal.Footer>
-      </Modal>
-    </div>;
-  }
-
-  onCreatingApp() {
-    this.setState({creatingApp: true});
-  }
-
-  onCreateAppModalClose() {
-    this.setState({creatingApp: false});
-  }
-
-  onCreatingAppNameEdited({target: {value}}) {
-    this.setState({creatingAppName: value});
-  }
-
-  onCreateApp() {
-    return requestJson('/apps', {
-      method: 'POST',
-      body: {
-        name: this.state.creatingAppName
-      }
-    }).then( app => {
-      this.setState({
-        creatingApp: false,
-        creatingAppName: ''
-      });
-
-      this.props.onAppCreated(app);
-    }).catch( err => {
-      alert(err.message);
-    });
-  }
-
-  onBuildingVersion(appName) {
-    this.setState({buildingVersion: appName});
-  }
-
-  onBuildVersionModalClose() {
-    this.setState({buildingVersion: false});
-  }
-
-  onBuildingVersionTagEdited({target: {value}}) {
-    this.setState({buildingVersionTag: value});
-  }
-
-  onBuildVersionDeployEdited({target: {checked}}) {
-    this.setState({buildingVersionDeploy: checked});
-  }
-
-  onBuildVersion() {
-    const {buildingVersion, buildingVersionDeploy} = this.state;
-
-    return requestJson(`/apps/${buildingVersion}/${buildingVersionDeploy ? 'version' : 'versions'}`, {
-      method: 'POST',
-      body: {
-        gitTag: this.state.buildingVersionTag
-      }
-    }).then( app => {
-      this.setState({
-        buildingVersion: false
-      });
-    }).catch( err => {
-      alert(err.message);
-    });
   }
 }
---
+++ frontend/components/console/application.jsx
@@ -0,0 +1,200 @@
+import _ from 'lodash';
+import {Button, Table, ButtonGroup, Modal, FormGroup, ControlLabel, FormControl, HelpBlock} from 'react-bootstrap';
+import {Label, DropdownButton, MenuItem, Checkbox} from 'react-bootstrap';
+import React, {Component} from 'react';
+
+import {requestJson} from '../../lib/request';
+
+export default class ApplicationsTab extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      editingApp: false,
+      buildingVersion: false
+    }
+  }
+
+  render() {
+    return <div>
+      <ButtonGroup>
+        <Button bsStyle='success' onClick={::this.onCreateApp}>Create App</Button>
+      </ButtonGroup>
+      <Table responsive>
+        <thead>
+          <tr>
+            <th>Domain</th>
+            <th>Version</th>
+            <th>Instances</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.apps.map( app => {
+            return <tr key={app.name}>
+              <td>{app.name}</td>
+              <td>
+                <Label bsStyle='primary'>{app.version}</Label>
+                <ButtonGroup>
+                  <Button onClick={this.onBuildingVersion.bind(this, app.name)}>Build</Button>
+                  <DropdownButton title='Deploy...' id='deploy-dropdown'>
+                    <MenuItem eventKey={1}>20170219-205301</MenuItem>
+                  </DropdownButton>
+                </ButtonGroup>
+              </td>
+              <td>{app.instances}</td>
+              <td>
+                <ButtonGroup>
+                  <Button onClick={this.onEditApp.bind(this, app.name)}>Edit</Button>
+                  <Button bsStyle='danger'>Delete</Button>
+                </ButtonGroup>
+              </td>
+            </tr>;
+          })}
+        </tbody>
+      </Table>
+
+      {this.state.editingApp && <EditAppModal {...this.state.editingApp} onClose={::this.onAppEdited} />}
+      {this.state.buildingVersion && <BuildVersionModal />}
+
+    </div>;
+  }
+
+  onCreateApp() {
+    this.setState({
+      editingApp: {}
+    });
+  }
+
+  onEditApp(name) {
+    this.setState({
+      editingApp: _.find(this.props.apps, {name}),
+    });
+  }
+
+  onAppEdited(app) {
+    this.setState({editingApp: false});
+    this.props.onAppEdited(app);
+  }
+
+  onBuildingVersion(appName) {
+    this.setState({buildingVersion: appName});
+  }
+
+  onBuildVersionModalClosed() {
+    this.setState({buildingVersion: false});
+  }
+}
+
+class EditAppModal extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {};
+  }
+
+  render() {
+    const title = this.props.name ? `Edit ${this.props.name}` : 'Create new app';
+
+    return <Modal show={true} onHide={::this.props.onClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>{title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <FormGroup controlId='app-name'>
+          <ControlLabel>Name</ControlLabel>
+          <FormControl disabled={this.props.name} type='text' {...this.linkField('name')} />
+          <HelpBlock>Used as your domain, must be globally unique</HelpBlock>
+        </FormGroup>
+        <FormGroup controlId='git-repository'>
+          <ControlLabel>Git Repository</ControlLabel>
+          <FormControl type='text' {...this.linkField('gitRepository')} />
+          <HelpBlock>Build version from this git repository</HelpBlock>
+        </FormGroup>
+      </Modal.Body>
+      <Modal.Footer>
+        {this.props.name ? null : <Button bsStyle='success' onClick={::this.onCreateApp}>Create</Button>}
+        {this.props.name && <Button bsStyle='success' onClick={::this.onEditApp}>Edit</Button>}
+      </Modal.Footer>
+    </Modal>;
+  }
+
+  linkField(field) {
+    return {
+      value: this.state[field] || this.props[field],
+      onChange: ({target: {value}}) => {
+        this.setState({[field]: value});
+      }
+    };
+  }
+
+  onCreateApp() {
+    return requestJson('/apps', {
+      method: 'POST',
+      body: this.state
+    }).then( app => {
+      this.props.onClose(app);
+    }).catch( err => {
+      alert(err.message);
+    });
+  }
+
+  onEditApp() {
+    return requestJson(`/apps/${this.props.name}`, {
+      method: 'PATCH',
+      body: this.state
+    }).then( app => {
+      this.props.onClose(app);
+    }).catch( err => {
+      alert(err.message);
+    });
+  }
+}
+
+class BuildVersionModal extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {};
+  }
+
+  render() {
+    return <Modal show={this.state.buildingVersion} onHide={::this.onBuildVersionModalClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Build new version</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <FormGroup controlId='git-tag'>
+          <ControlLabel>App</ControlLabel>
+          <FormControl type='text' disabled={true} value={this.state.buildingVersion}  />
+        </FormGroup>
+        <FormGroup controlId='git-tag'>
+          <ControlLabel>Git tag</ControlLabel>
+          <FormControl type='text' onChange={::this.onBuildingVersionTagEdited} value={this.state.buildingVersionTag}  />
+          <HelpBlock>Git branch, tag or commit hash</HelpBlock>
+        </FormGroup>
+        <Checkbox onChange={::this.onBuildVersionDeployEdited} value={this.state.buildingVersionDeploy}>Deploy to app after build finished</Checkbox>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button bsStyle='success' onClick={::this.onBuildVersion}>Build</Button>
+      </Modal.Footer>
+    </Modal>;
+  }
+
+  onBuildVersion() {
+    const {buildingVersion, buildingVersionDeploy} = this.state;
+
+    return requestJson(`/apps/${buildingVersion}/${buildingVersionDeploy ? 'version' : 'versions'}`, {
+      method: 'POST',
+      body: {
+        gitTag: this.state.buildingVersionTag
+      }
+    }).then( app => {
+      this.setState({
+        buildingVersion: false
+      });
+    }).catch( err => {
+      alert(err.message);
+    });
+  }
+}

17cdaf8a7555fa8419fe49621d33c68d9dc93ec3
Mason Zhang <zzy228961125@gmail.com>
2017-08-27 23:44:03 +0800

    :ok_hand: Fixing bugs of working dir

--- lib/utils/asset.go
+++ lib/utils/asset.go
@@ -6,10 +6,13 @@ import (
 )
 
 func GetAssetFilePath(relatedPath string) (string, error) {
+	if dir := os.Getenv("WORKDIR"); dir != "" {
+		return filepath.Join(dir, relatedPath), nil
+	}
+
 	workDir, err := os.Getwd()
 	if err != nil {
 		return "", err
 	}
-
 	return filepath.Join(workDir, relatedPath), nil
 }

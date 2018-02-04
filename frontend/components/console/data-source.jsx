9ece6d2b2aae2f88e502d52701aee0745984b3ff
jysperm <jysperm@gmail.com>
2018-02-04 18:13:11 +0800

    :sparkles: Frontend: edit dataSrouce links and delete dataSource

--- frontend/components/console/data-source.jsx
+++ frontend/components/console/data-source.jsx
@@ -1,6 +1,7 @@
-import React, {Component} from 'react';
 import {Button, Table, ButtonGroup, Modal} from 'react-bootstrap';
-import {FormGroup, ControlLabel, FormControl, HelpBlock} from 'react-bootstrap';
+import {FormGroup, ControlLabel, FormControl, HelpBlock, Checkbox} from 'react-bootstrap';
+import React, {Component} from 'react';
+import _ from 'lodash';
 
 import {alertError} from '../../lib/error';
 import {FormComponent} from '../../lib/components';
@@ -8,11 +9,12 @@ import {requestJson} from '../../lib/request';
 
 export default class DataSourcesTab extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      editingDataSource: null
-    }
+      editingDataSource: null,
+      editingLinks: null
+    };
   }
 
   render() {
@@ -33,6 +35,7 @@ export default class DataSourcesTab extends Component {
         <tbody>
           {this.props.dataSources.map( dataSource => {
             return <tr key={dataSource.name}>
+              <td>{dataSource.name}</td>
               <td>{dataSource.type}</td>
               <td>{dataSource.instances}</td>
               <td>
@@ -49,6 +52,7 @@ export default class DataSourcesTab extends Component {
       </Table>
 
       {this.state.editingDataSource && <EditDataSourceModal {...this.state.editingDataSource} onClose={::this.onDataSourceEdited} />}
+      {this.state.editingLinks && <EditLinksModal apps={this.props.apps} dataSource={this.state.editingLinks} onClose={::this.onLinksEdited} />}
 
     </div>;
   }
@@ -68,17 +72,30 @@ export default class DataSourcesTab extends Component {
   }
 
   onEditingLinks(dataSourceName) {
-
+    this.setState({
+      editingLinks: _.find(this.props.dataSources, {name: dataSourceName})
+    });
   }
 
-  onDeleteDataSource(dataSourceName) {
+  onLinksEdited() {
+    this.setState({
+      editingLinks: null
+    });
+  }
 
+  onDeleteDataSource(name) {
+    return requestJson(`/data-sources/${name}`, {
+      method: 'DELETE',
+      body: this.state
+    }).then( () => {
+      this.props.onDataSourceDeleted({name});
+    }).catch(alertError);
   }
 }
 
 class EditDataSourceModal extends FormComponent {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {};
   }
@@ -119,6 +136,52 @@ class EditDataSourceModal extends FormComponent {
   }
 }
 
-class EditDataSourceLinksModal extends FormComponent {
+class EditLinksModal extends FormComponent {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      linkedApps: _.clone(this.props.dataSource.linkedApps)
+    };
+  }
+
+  render() {
+    return <Modal show={true} onHide={this.props.onClose.bind(this, null)}>
+      <Modal.Header closeButton>
+        <Modal.Title>Link {this.props.dataSource.name} to ...</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {this.props.apps.map( app => {
+          let checked = _.includes(this.state.linkedApps, app.name);
+
+          return <Checkbox key={app.name} value={checked} onChange={this.onCheckChanged.bind(this, app.name)}>
+            {app.name}
+          </Checkbox>;
+        })}
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={::this.onCloseClicked}>Close</Button>
+      </Modal.Footer>
+    </Modal>;
+  }
+
+  onCheckChanged(appName, {target}) {
+    return requestJson(`/data-sources/${this.props.dataSource.name}/links/${appName}`, {
+      method: target.checked ? 'POST' : 'DELETE'
+    }).then( () => {
+      if (target.checked) {
+        this.setState({
+          linkedApps: _.union(this.state.linkedApps, [appName])
+        });
+      } else {
+        this.setState({
+          linkedApps: _.without(this.state.linkedApps, appName)
+        });
+      }
+    }).catch(alertError);
+  }
+
+  onCloseClicked() {
+    this.props.onClose();
+  }
 }

5df5de53869de4d845747724b6671485b0971881
jysperm <jysperm@gmail.com>
2018-02-18 22:09:44 +0800

    :recycle: Refactor login frontend

--- frontend/components/login.jsx
+++ frontend/components/login.jsx
@@ -1,79 +1,78 @@
-import React, {Component} from 'react';
-import {hashHistory} from 'react-router'
-import {Row, Col, Tabs, Tab, Form, FormGroup, FormControl, ControlLabel, Button} from 'react-bootstrap';
 import _ from 'lodash';
+import {Row, Col, Tabs, Tab, Form, FormGroup, FormControl, ControlLabel, Button} from 'react-bootstrap';
+import React, {Component} from 'react';
 
-import {requestJson} from '../lib/request'
-import LayoutView from './layout'
+import {alertError} from '../lib/error';
+import {FormComponent} from '../lib/components';
+import {requestJson} from '../lib/request';
+import LayoutView from './layout';
 
 export default class LoginView extends Component {
   render() {
     return <LayoutView pageHeader={true}>
       <Row>
-        <Tabs defaultActiveKey={2} id='signup-or-login'>
-          <Tab eventKey={1} title='Sign up'>
-            <SignupTab />
+        <Tabs animation={false} defaultActiveKey='login' id='signup-or-login'>
+          <Tab eventKey='signup' title='Sign up'>
+            <SignupTab onLogged={::this.onLogged} />
           </Tab>
-          <Tab eventKey={2} title='Login'>
-            <LoginTab />
+          <Tab eventKey='login' title='Login'>
+            <LoginTab onLogged={::this.onLogged} />
           </Tab>
         </Tabs>
       </Row>
     </LayoutView>;
   }
+
+  onLogged(session) {
+    localStorage.setItem('username', session.username);
+    localStorage.setItem('sessionToken', session.token);
+    this.props.history.push('/console');
+  }
 }
 
-class SignupTab extends Component {
+class SignupTab extends FormComponent {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       username: '',
       email: '',
       password: ''
-    }
+    };
   }
 
   render() {
-    const changeState = (field) => {
-      return ({target: {value}}) => {
-        this.setState({
-          [field]: value
-        });
-      };
-    };
-
     return <Form horizontal>
-      <FormGroup controlId='username'>
+      <FormGroup controlId='signup-username'>
         <Col componentClass={ControlLabel} sm={2}>
           Username
         </Col>
         <Col sm={10}>
-          <FormControl type='text' value={this.state.username} onChange={changeState('username')} />
+          <FormControl type='text' {...this.linkField('username')} />
         </Col>
       </FormGroup>
 
-      <FormGroup controlId='email'>
+      <FormGroup controlId='signup-email'>
         <Col componentClass={ControlLabel} sm={2}>
           Email
         </Col>
         <Col sm={10}>
-          <FormControl type='email' value={this.state.email} onChange={changeState('email')} />
+          <FormControl type='email' {...this.linkField('email')} />
         </Col>
       </FormGroup>
 
-      <FormGroup controlId='password'>
+      <FormGroup controlId='signup-password'>
         <Col componentClass={ControlLabel} sm={2}>
           Password
         </Col>
         <Col sm={10}>
-          <FormControl type='password' value={this.state.password} onChange={changeState('password')} />
+          <FormControl type='password' {...this.linkField('password')} />
         </Col>
       </FormGroup>
 
       <FormGroup>
         <Col smOffset={2} sm={10}>
-          <Button type='button' bsStyle='primary' onClick={this.onSubmit.bind(this)}>
+          <Button type='button' bsStyle='primary' onClick={::this.onSubmit}>
             Sign up
           </Button>
         </Col>
@@ -90,57 +89,45 @@ class SignupTab extends Component {
         method: 'POST',
         body: _.pick(this.state, 'username', 'password')
       }).then( session => {
-        localStorage.setItem('username', session.username);
-        localStorage.setItem('sessionToken', session.token);
-        hashHistory.push('/console');
+        this.props.onLogged(session);
       });
-    }).catch( err => {
-      alert(err.message);
-    });
+    }).catch(alertError);
   }
 }
 
-class LoginTab extends Component {
+class LoginTab extends FormComponent {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       username: '',
       password: ''
-    }
+    };
   }
 
   render() {
-    const changeState = (field) => {
-      return ({target: {value}}) => {
-        this.setState({
-          [field]: value
-        });
-      };
-    };
-
     return <Form horizontal>
-      <FormGroup controlId='username'>
+      <FormGroup controlId='login-username'>
         <Col componentClass={ControlLabel} sm={2}>
           Username
         </Col>
         <Col sm={10}>
-          <FormControl type='text' value={this.state.username} onChange={changeState('username')} />
+          <FormControl type='text' {...this.linkField('username')} />
         </Col>
       </FormGroup>
 
-      <FormGroup controlId='password'>
+      <FormGroup controlId='login-password'>
         <Col componentClass={ControlLabel} sm={2}>
           Password
         </Col>
         <Col sm={10}>
-          <FormControl type='password' value={this.state.password} onChange={changeState('password')} />
+          <FormControl type='password' {...this.linkField('password')} />
         </Col>
       </FormGroup>
 
       <FormGroup>
         <Col smOffset={2} sm={10}>
-          <Button type='button' bsStyle='primary' onClick={this.onSubmit.bind(this)}>
+          <Button type='button' bsStyle='primary' onClick={::this.onSubmit}>
             Login
           </Button>
         </Col>
@@ -153,11 +140,7 @@ class LoginTab extends Component {
       method: 'POST',
       body: _.pick(this.state, 'username', 'password')
     }).then( session => {
-      localStorage.setItem('username', session.username);
-      localStorage.setItem('sessionToken', session.token);
-      hashHistory.push('/console');
-    }).catch( err => {
-      alert(err.message);
-    });
+      this.props.onLogged(session);
+    }).catch(alertError);
   }
 }

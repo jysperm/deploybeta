cf021c456b730557c7a713d87bc9dcb834b21a81
jysperm <jysperm@gmail.com>
2018-02-20 21:15:19 +0800

    :art: Use shorthand in routes.go

--- web/routes.go
+++ web/routes.go
@@ -10,32 +10,36 @@ import (
 func CreateWebServer() *echo.Echo {
 	app := echo.New()
 
+	auth := helpers.AuthenticateMiddleware
+	appOwner := helpers.AppOwnerMiddleware
+	dataSource := helpers.DataSourceMiddleware
+
 	app.File("/", "./frontend/public/index.html")
 	app.Static("/assets", "./frontend/public")
 
 	app.POST("/accounts", handlers.RegisterAccount)
 	app.POST("/sessions", handlers.CreateSession)
 
-	app.GET("/session/account", handlers.CurrentAccount, helpers.AuthenticateMiddleware)
+	app.GET("/session/account", handlers.CurrentAccount, auth)
 
-	app.POST("/apps/:name/versions", handlers.CreateVersion, helpers.AuthenticateMiddleware, helpers.AppOwnerMiddleware)
-	app.PUT("/apps/:name/version", handlers.DeployVersion, helpers.AuthenticateMiddleware, helpers.AppOwnerMiddleware)
-	app.GET("/apps/:name/versions/:tag/progress", handlers.PushProgress, helpers.AuthenticateMiddleware, helpers.AppOwnerMiddleware)
+	app.POST("/apps/:name/versions", handlers.CreateVersion, auth, appOwner)
+	app.PUT("/apps/:name/version", handlers.DeployVersion, auth, appOwner)
+	app.GET("/apps/:name/versions/:tag/progress", handlers.PushProgress, auth, appOwner)
 
-	app.GET("/apps", handlers.GetMyApps, helpers.AuthenticateMiddleware)
-	app.POST("/apps", handlers.CreateApp, helpers.AuthenticateMiddleware)
-	app.PATCH("/apps/:name", handlers.UpdateApp, helpers.AuthenticateMiddleware, helpers.AppOwnerMiddleware)
-	app.DELETE("/apps/:name", handlers.DeleteApp, helpers.AuthenticateMiddleware)
+	app.GET("/apps", handlers.GetMyApps, auth)
+	app.POST("/apps", handlers.CreateApp, auth)
+	app.PATCH("/apps/:name", handlers.UpdateApp, auth, appOwner)
+	app.DELETE("/apps/:name", handlers.DeleteApp, auth, appOwner)
 
-	app.GET("/data-sources", handlers.ListDataSources, helpers.AuthenticateMiddleware)
-	app.POST("/data-sources", handlers.CreateDataSource, helpers.AuthenticateMiddleware)
-	app.PATCH("/data-sources/:name", handlers.UpdateDataSource, helpers.AuthenticateMiddleware, helpers.DataSourceMiddleware)
-	app.DELETE("/data-sources/:name", handlers.DeleteDataSource, helpers.AuthenticateMiddleware, helpers.DataSourceMiddleware)
+	app.GET("/data-sources", handlers.ListDataSources, auth)
+	app.POST("/data-sources", handlers.CreateDataSource, auth)
+	app.PATCH("/data-sources/:name", handlers.UpdateDataSource, auth, dataSource)
+	app.DELETE("/data-sources/:name", handlers.DeleteDataSource, auth, dataSource)
 
-	app.POST("/data-sources/:name/agents", handlers.CreateDataSourceNode, helpers.AuthenticateMiddleware, helpers.DataSourceMiddleware)
+	app.POST("/data-sources/:name/links/:appName", handlers.LinkDataSource, auth, dataSource)
+	app.PUT("/data-sources/:name/links/:appName", handlers.UnlinkDataSource, auth, dataSource)
 
-	app.POST("/data-sources/:name/links/:appName", handlers.LinkDataSource, helpers.AuthenticateMiddleware, helpers.DataSourceMiddleware)
-	app.PUT("/data-sources/:name/links/:appName", handlers.UnlinkDataSource, helpers.AuthenticateMiddleware, helpers.DataSourceMiddleware)
+	app.POST("/data-sources/:name/agents", handlers.CreateDataSourceNode)
 
 	return app
 }
